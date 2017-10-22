3835e918fb9789e2649cef2f6cdd8d92f0fbf71f
frank870622 <frank870622@gmail.com>
2017-10-22 17:07:30 +0800

    Add files via upload

--- HW2.c
+++ HW2.c
@@ -1,9 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 int n = 1;
-int board[8][8] = {0};
+int board[8][8];
 int movey[7] = {1,2,2,1,-1,-2,-2,-1};
 int movex[7] = {-2,-1,1,2,2,1,-1,-2};
+
+int decideroad(int npos[],int modehistory[]){
+    int way = 9;
+    int mode = -1;
+    for(int i=0;i<8;++i){
+        if(npos[i] != -1 ){
+            if(npos[i] < way){
+                way = npos[i];
+                mode = i;
+            }
+        }
+    }
+    for (int i=0;i<64;++i){
+        if(npos[modehistory[i]] == way){
+            mode = modehistory[i];
+            break;
+        }
+    }
+    return mode;
+}
+
+int numberofnpos(int x,int y){
+    int npos = 0;
+    int nextx[7];
+    int nexty[7];
+    for(int i=0;i<8;++i){
+        nextx[i] = x + movex[i];
+        nexty[i] = y + movey[i];
+    }
+    for(int i=0;i<8;++i){
+        if((nextx[i]>=0) && (nextx[i]<n) && (nexty[i]>=0) && (nexty[i]<n) && (board[nextx[i]][nexty[i]]) == 0){
+           ++npos;
+        }
+    }
+    return npos;
+}
+
 void findnpos(int *x,int *y){
     int nextx[7];
     int nexty[7];
@@ -37,40 +74,7 @@ void findnpos(int *x,int *y){
     }
 
 }
-int numberofnpos(int x,int y){
-    int npos = 0;
-    int nextx[7];
-    int nexty[7];
-    for(int i=0;i<8;++i){
-        nextx[i] = x + movex[i];
-        nexty[i] = y + movey[i];
-    }
-    for(int i=0;i<8;++i){
-        if((nextx[i]>=0) && (nextx[i]<n) && (nexty[i]>=0) && (nexty[i]<n) && (board[nextx[i]][nexty[i]]) == 0){
-           ++npos;
-        }
-    }
-    return npos;
-}
-int decideroad(int npos[],int modehistory[]){
-    int way = 9;
-    int mode = -1;
-    for(int i=0;i<8;++i){
-        if(npos[i] != -1 ){
-            if(npos[i] < way){
-                way = npos[i];
-                mode = i;
-            }
-        }
-    }
-    for (int i=0;i<64;++i){
-        if(npos[modehistory[i]] == way){
-            mode = modehistory[i];
-            break;
-        }
-    }
-    return mode;
-}
+
 int main(){
     for (;n<=8;++n){
         for(int i=0;i<8;++i) {
