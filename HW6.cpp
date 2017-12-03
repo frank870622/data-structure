1eba4e8bcf6025fdea75712df70985c97ba6a27c
frank870622 <frank870622@gmail.com>
2017-12-03 15:06:00 +0800

    Add files via upload

---
+++ HW6.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int findheight(int parent[],int x){
+    for(;parent[x] >= 0;x = parent[x]);
+    return parent[x];
+}
+void heightunion(int parent[],int i,int j,int roota,int rootb){
+    int temp = roota + rootb;
+    if(roota > rootb){
+        parent[i] = j;
+        parent[j] = temp;
+    }
+    else{
+        parent[j] = i;
+        parent[i] = temp;
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    int parent[n] = {-1};
+    for(int i=0;i<n;++i)    parent[i] = -1;
+    for(int i=1;i<n;++i){
+        heightunion(parent,0,i,findheight(parent,0),findheight(parent,i));
+    }
+    cout << "NODE   parent\n";
+    for(int i=0;i<n;++i){
+        cout << i << "\t";
+        if (parent[i] < 0)  {
+            cout << "root\n";
+            continue;
+        }
+        cout << parent[i] << "\n";
+    }
+    return 0;
+}
---
+++ readme(HW6).txt
@@ -0,0 +1,18 @@
+F74056166 方嘉祥  資訊109 資料結構 HW6
+(a)
+you can imput a number to determine the number of nodes
+the heightunion function has four input
+	parent[],two node number(i,j),the height of two node;
+if height i > height j		make i the parent of j
+else 				make j the parent of i,
+
+during print	if parent[n] < 0 
+		we print "root"
+
+this function run in O(1) time
+
+(d)
+
+in situation 1 heightUnion produces better results
+
+in situation 2 weightedUnion produces better results
